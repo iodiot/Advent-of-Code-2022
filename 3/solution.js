5706f731278ae907060d4d0aadbb93ec517ef248
iodiot <iodiot@gmail.com>
2022-12-04 12:15:08 +0200

    Solutions for Day 3 & 4

--- 3/solution.js
+++ 3/solution.js
@@ -1,10 +1,37 @@
-//
+// --- Day 3: Rucksack Reorganization ---
 
 import fs from 'fs';
 
-const input = fs.readFileSync('./input.txt', 'utf8').trim();
+function get_priority(ch) {
+  const val = ch.charCodeAt(0);
+  return val <= 'Z'.charCodeAt(0) ? val - 'A'.charCodeAt(0) + 27 : val - 'a'.charCodeAt(0) + 1;
+}
 
-// console.log(input);
+function count_sum(rucksacks) {
+  return rucksacks.map(items => {
+    const parts = items.split('|');
 
-console.log(`part 1: ${ 0 }`);
-console.log(`part 2: ${ 0 }`);
+    const intersect = parts.reduce((a, b) => {
+      return a.toString().split('').filter(item => b.split('').includes(item));
+    })[0];
+
+    return get_priority(intersect);
+  }).reduce((total, n) => total + n);
+}
+
+const input = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
+
+const rucksacks_1 = input.map(line => line.slice(0, line.length / 2) + '|' + line.slice(-line.length / 2));
+
+let rucksacks_2 = [];
+
+for (let i = 0; i < input.length; i += 3) {
+  rucksacks_2.push(input.slice(i, i + 3).join('|'));
+}
+
+console.time();
+
+console.log(`part 1: ${ count_sum(rucksacks_1) }`);
+console.log(`part 2: ${ count_sum(rucksacks_2) }`);
+
+console.timeEnd();
--- 4/solution.js
+++ 4/solution.js
@@ -1,10 +1,22 @@
-//
+// --- Day 4: Camp Cleanup ---
 
 import fs from 'fs';
 
 const input = fs.readFileSync('./input.txt', 'utf8').trim();
 
-// console.log(input);
+const lines = input.split('\n').map(line => line.match(/\d+/g).map(s => +s));
 
-console.log(`part 1: ${ 0 }`);
-console.log(`part 2: ${ 0 }`);
+const fully = lines.filter(line => {
+  return (line[0] >= line[2] && line[1] <= line[3]) || (line[2] >= line[0] && line[3] <= line[1]);
+}).length;
+
+const not_fully = lines.filter(line => {
+  return (line[2] <= line[1]) && (line[3] >= line[0]);
+}).length;
+
+console.time();
+
+console.log(`part 1: ${ fully }`);
+console.log(`part 2: ${ not_fully }`);
+
+console.timeEnd();
