4016bf8d8aa545f0b3f2454795ac2f8e8ee8b0f4
iodiot <iodiot@gmail.com>
2022-12-06 07:45:08 +0200

    Solve `Day 6: Tuning Trouble`

--- 6/solution.js
+++ 6/solution.js
@@ -1,14 +1,27 @@
-//
+// --- Day 6: Tuning Trouble ---
 
 import fs from 'fs';
 
 const input = fs.readFileSync('./input.txt', 'utf8').trim();
 
-// console.log(input);
+function find_start(input, distinct = 4) {
+  const chunk = input.slice(0, distinct).split('');
+
+  for (let i = distinct; i < input.length; ++i) {
+    if (new Set(chunk).size === distinct) {
+      return i;
+    }
+
+    chunk.shift();
+    chunk.push(input[i]);
+  }
+
+  return -1;
+}
 
 console.time();
 
-console.log(`part 1: ${ 0 }`);
-console.log(`part 2: ${ 0 }`);
+console.log(`part 1: ${ find_start(input) }`);
+console.log(`part 2: ${ find_start(input, 14) }`);
 
 console.timeEnd();
---
+++ 7/solution.js
@@ -0,0 +1,14 @@
+//
+
+import fs from 'fs';
+
+const input = fs.readFileSync('./input.txt', 'utf8').trim();
+
+// console.log(input);
+
+console.time();
+
+console.log(`part 1: ${ 0 }`);
+console.log(`part 2: ${ 0 }`);
+
+console.timeEnd();
